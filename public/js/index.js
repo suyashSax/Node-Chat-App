f666f2fd24c2327903d57dbdff90a51e9c36c828
suyashSax <suyash2099@gmail.com>
2017-07-05 13:22:31 +0400

    Autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,22 @@ var socket = io()
 
 // Arrow funcs not fully compatible with mobile clients...
 
+function scrollToBottom(){
+    // Selectors
+    var messages = jQuery('#messages')
+    var newMessage = messages.children('li:last-child')
+    // Heights
+    var clientHeight = messages.prop('clientHeight')
+    var scrollTop = messages.prop('scrollTop')
+    var scrollHeight = messages.prop('scrollHeight')
+    var newMessageHeight = newMessage.innerHeight()
+    var lastMessageHeight = newMessage.prev().innerHeight()
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight)
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to Server')
 })
@@ -24,11 +40,7 @@ socket.on('receive', function (message) {
     })
 
     jQuery('#messages').append(html)
-
-    // var li = jQuery('<li></li>')
-    // li.text(`${message.from} ${formattedTime}: ${message.text}`)
-    //
-    // jQuery('#messages').append(li)
+    scrollToBottom()
 })
 
 socket.on('receiveLocation', function (message){
@@ -42,14 +54,8 @@ socket.on('receiveLocation', function (message){
         createdAt: formattedTime
     })
 
-    // var li = jQuery('<li></li>')
-    // var formattedTime = moment(message.createdAt).format('h:mm a')
-    // var a = jQuery('<a target="_blank">My current location</a>')
-    // li.text(`${message.from} ${formattedTime}: `)
-    // a.attr('href', message.url)
-    // li.append(a)
-
     jQuery('#messages').append(html)
+    scrollToBottom()
 })
 
 jQuery('#message-form').on('submit', function (e) {
